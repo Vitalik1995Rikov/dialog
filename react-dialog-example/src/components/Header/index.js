3b9b3e09d25b5fb86fb1fb1a56b69ef3349509dd
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-11-30 21:53:44 +0300

    add Header

---
+++ react-dialog-example/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserInfo from './UserInfo';
+
+import './styles';
+
+cosnt Header = () => {
+    return (
+        <div className="header">
+            <UserInfo />
+        </div>
+    )
+}
+
+export default Header;
