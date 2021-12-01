ca5e5ae0fed89f4335f9ba8ce402668cc6bd345c
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-01 20:44:07 +0300

    useEffect2

--- react-dialog-example/src/App.js
+++ react-dialog-example/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const App = (props) => {
 
 const [isReversed, reverse] = useState(null);
 const [color, changeColor] = useState(0);
+const [renderCount, setRenderCount] = useState(1);
 
 const colors = ["#001f3f", "#39cccc", "#39c5cc", "#d9cccc"];
 const styles = {color: colors[color]}
@@ -15,10 +16,19 @@ const onClickHandle = (e) => {
   changeColor(i => i + 1)
 }
 
+useEffect(() => {
+    if (isReversed !== null) {
+      setRenderCount(count => count + 1)
+    }
+}, [isReversed])
+
   return (
-      <p onClick={onClickHandle} style={styles}>
-        {isReversed ? props.text.split('').reverse().join('') : props.text}
-      </p>
+      <div>
+        <p onClick={onClickHandle} style={styles}>
+          {isReversed ? props.text.split('').reverse().join('') : props.text}
+        </p>
+        Render {renderCount} раз
+      </div>
   );
 }
 
