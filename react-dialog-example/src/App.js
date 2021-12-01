4fece5b3f00e4d7f07e68aed63e6f780c24b3541
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-01 20:29:42 +0300

    useState2

--- react-dialog-example/src/App.js
+++ react-dialog-example/src/App.js
@@ -4,15 +4,21 @@ import React, { useState } from "react";
 const App = (props) => {
 
 const [isReversed, reverse] = useState(null);
+const [color, changeColor] = useState(0);
+
+const colors = ["#001f3f", "#39cccc", "#39c5cc", "#d9cccc"];
+const styles = {color: colors[color]}
+
 
 const onClickHandle = (e) => {
   reverse(isReversed => !isReversed);
+  changeColor(i => i + 1)
 }
 
   return (
-    <p onClick={onClickHandle}>
-      {isReversed ? props.text.split('').reverse().join('') : props.text}
-    </p>
+      <p onClick={onClickHandle} style={styles}>
+        {isReversed ? props.text.split('').reverse().join('') : props.text}
+      </p>
   );
 }
 
