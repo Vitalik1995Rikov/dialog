09de13d88b55a1bd0e76f0417d3e129499583ef9
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-05 00:43:09 +0300

    styled components

--- react-dialog-example/src/App.js
+++ react-dialog-example/src/App.js
@@ -6,16 +6,25 @@ import Contact from "./Contact";
 import AllContacts from "./AllContacts";
 import Home from "./Home";
 import User from "./User";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const App = () => (
   <main>
-    <User/>
-    <Routes>
-      <Route path="/about" element={<About />}></Route>
-      <Route path="/contact/:id" element={<Contact />}></Route>
-      <Route path="/contact" element={<AllContacts />}></Route>
-      <Route path="/" element={<Home />}></Route>
-    </Routes>
+    <Wrapper>
+      <User/>
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact/:id" element={<Contact />}></Route>
+          <Route path="/contact" element={<AllContacts />}></Route>
+          <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Wrapper>
   </main>
 )
 
