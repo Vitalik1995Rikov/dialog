22fbbc5a188fb0fbd6c7d864136563237c72439f
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-12-06 20:24:19 +0300

    ReactRouter

--- react-dialog-example/src/index.js
+++ react-dialog-example/src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 
-import App from './App';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navigation/>
+      <Route exact path="/one">
+        <One/>
+      </Route>
+      <Route exact path="/two">
+        <Two/>
+      </Route>
+    </BrowserRouter>
+  )
+}
+
+function One() {
+
+}
+
+function Two() {
+
+}
+
+function Navigation() {
+
+}
 
 ReactDOM.render(
   <BrowserRouter>
